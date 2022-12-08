1f3f97aabc5d332198ea8e8cd626b661712ea2f5
Yeonji Choi <duswl8936@naver.com>
2022-12-08 21:44:38 +0900

    공용input onChange와 useState 구현 할 예정

---
+++ src/components/InputPublic.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function InputPublic() {
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="ID"
+          class="border-solid border-2"
+          value="ID"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default InputPublic;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Button from '../components/Button.jsx'
-import Background from '../components/Background.jsx';
+import React from "react";
+import Button from "../components/Button.jsx";
+import Background from "../components/Background.jsx";
+import InputPublic from "../components/InputPublic.jsx";
 
 function Login(props) {
-    return (
-        <div>
-            <Background></Background>
-            <Button></Button>
-        </div>
-    );
+  return (
+    <div>
+      <Background></Background>
+      <Button></Button>
+      <InputPublic></InputPublic>
+    </div>
+  );
 }
 
 export default Login;
