5b07ea7f612e1bfb68937ab96884aacf16c2bc83
wisdomyeon <duswl8936@naver.com>
2022-12-10 19:33:54 +0900

    modal창 구현

---
+++ src/components/ModalButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+import Background from './Background';
+
+function ModalButton(props) {
+    let [modal, setModal] = useState(true);
+    return (
+        <div>
+            <button class="w-14 h-14 rounded-full  bg-buttonBg" onClick={() => {
+                setModal(!modal);
+            }}>버튼</button>
+            {modal == true ? <Background></Background> : null}
+        </div>
+    );
+}
+
+export default ModalButton;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Background from "../components/Background.jsx";
 import Button from "../components/Button.jsx";
+import ModalButton from "../components/ModalButton.jsx";
 
 function Login(props) {
     let [inputs, setInputs] = useState({ id: "", password: "" });
@@ -11,9 +12,9 @@ function Login(props) {
     }
     return (
         <div>
-            <Background></Background>
+            <ModalButton></ModalButton>
             <input type="text" name="id" value={id} placeholder="아이디를 입력하세요" class="border-solid border-2" onChange={onChangeInput} />
-            <input type="password" name="password" placeholder="비밀번호를 입력하세요" class="border-solid border-2" onChange={onChangeInput} />
+            <input type="password" name="password" value={password} placeholder="비밀번호를 입력하세요" class="border-solid border-2" onChange={onChangeInput} />
             <Button inputs={inputs}></Button>
 
         </div>
